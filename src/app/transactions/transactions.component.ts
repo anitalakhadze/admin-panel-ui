a9f62d4004fc72d6bbaddd0014b2486678a6f71b
a.talakhadze <a.talakhadze@oppa.ge>
2021-11-19 08:36:17 +0400

    Minor modifications to transactions component.

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {Transaction} from "../interfaces";
 import {ApiService} from "../service/api.service";
@@ -6,6 +6,9 @@ import {NotificationService} from "../utils/notification.service";
 import {ExcelService} from "../utils/excel.service";
 import {MatDialog} from "@angular/material/dialog";
 import {TransactionDetailsComponent} from "../transaction-details/transaction-details.component";
+import {MatSort} from "@angular/material/sort";
+import {filter} from "rxjs/operators";
+import {SnackbarService} from "../utils/snackbar.service";
 
 @Component({
   selector: 'app-transactions',
@@ -15,19 +18,29 @@ import {TransactionDetailsComponent} from "../transaction-details/transaction-de
 export class TransactionsComponent implements OnInit {
 
   transactionsDataSource = new MatTableDataSource<Transaction>([]);
-  displayedColumns: string[] = ['transactionId', 'amount', 'commission', 'invoiceData', 'dateCreated', 'details'];
+  displayedColumns: string[] = ['id', 'amount', 'commission', 'invoiceData', 'dateCreated', 'details'];
   fileName = 'ტრანზაქციები';
   transactionsList?: Transaction[];
+  public exportButtonLoading: boolean = false;
+
+  @ViewChild(MatSort) set matSort(sort: MatSort) {
+    this.transactionsDataSource.sort = sort;
+  }
 
   constructor(
     private apiService: ApiService,
     private notificationService: NotificationService,
     private excelService: ExcelService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private snackbarService: SnackbarService,
   ) { }
 
   ngOnInit(): void {
     this.getTransactions();
+  }
+
+  applyFilter(filterValue: KeyboardEvent) {
+    this.transactionsDataSource.filter = (filterValue.target as HTMLInputElement).value.trim().toLocaleLowerCase();
   }
 
   getTransactions(): void {
@@ -37,15 +50,18 @@ export class TransactionsComponent implements OnInit {
           this.transactionsDataSource = new MatTableDataSource<Transaction>(data);
           this.transactionsList = data;
           console.table(data);
-          this.notificationService.displaySuccessMessage();
+          this.snackbarService.openSnackBar('მონაცემების ჩატვირთვა დასრულდა წარმატებით')
+        }, error => {
+          this.snackbarService.openSnackBar('მონაცემების ჩატვირთვა დასრულდა წარმატების უგარეშოდ')
         }
       )
   }
 
   downloadExcel() {
+    this.exportButtonLoading = true;
     this.excelService.exportToExcel(this.fileName, this.transactionsList);
+    this.exportButtonLoading = false;
   }
-
   showDetails(data: any) {
     return this.dialog.open(TransactionDetailsComponent, {
       width: '300px',
